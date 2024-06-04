88a4c7a7deba1e114d1a327ccc8da29166b1e8a1
Manash <Manash@DESKTOP-H7BTIBJ>
2024-06-04 07:39:40 +0530

    Completion of Assignment 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import {Products} from "./components/ListItem/Products";
-
+import { Header } from "./components/Header";
 
 const App = () => {
   return (
     <div className="App">
      {/* <h1> hello world!</h1> */}
+     <Header/>
      <Products></Products>
     </div>
   );
