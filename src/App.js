bc47421b0d9817446c81d6ba2e5bfd30f6aa9a37
Manash <Manash@DESKTOP-H7BTIBJ>
2024-06-04 06:48:01 +0530

    Completion of Styling Components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ListItem from "./components/ListItem";
 const App = () => {
   return (
     <div className="App">
-     <h1> hello world!</h1>
+     {/* <h1> hello world!</h1> */}
      <ListItem></ListItem>
     </div>
   );
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,20 +1,21 @@
 import AddToCartIcon from "../assets/icons/add_cart.svg"
+
 const ListItem = ()=>{
     return (
-        <div>
-            <img src="../../../assets/placeholder.png" alt="some title" />
-            <div>
-                <div>
+        <div className={"item-card"}>
+            <img className={"img-fluid"} src="../../../assets/placeholder.png" alt="some title" />
+            <div className={"item-card__information"}>
+                <div className={"pricing"}>
                     <span>₹340</span>
                     <small>
                        <strike>450</strike>
                      </small>
                 </div>
-                <div>
+                <div className="title">
                     <h3>Title of the item </h3>
                 </div>
             </div>
-            <button>
+            <button className={"cart-add"}>
                 <span>Add to Card</span>
                 <img src={AddToCartIcon} alt="add to cart"/>
             </button>
