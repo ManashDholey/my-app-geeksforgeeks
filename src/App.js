a63de26e610a32c6ed1df48ddee150c66a12e2dc
Manash <Manash@DESKTOP-H7BTIBJ>
2024-06-04 16:38:46 +0530

    complition of Form Handling & Multiple States

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import {Products} from "./components/ListItem/Products";
+import {Products} from "./components/Products";
 import { Header } from "./components/layout/Header";
 import { SubHeader } from "./components/layout/SubHeader";
 
 const App = () => {
   return (
     <div className="App">
-     {/* <h1> hello world!</h1> */}
      <Header/>
      <SubHeader/>
      <Products></Products>
