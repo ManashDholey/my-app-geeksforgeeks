5217b56b6d0a3876b4a9422e5dae53cf80bb6c0f
Manash <Manash@DESKTOP-H7BTIBJ>
2024-06-04 06:56:09 +0530

    completion of dynamic data in component

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,18 +1,24 @@
 import AddToCartIcon from "../assets/icons/add_cart.svg"
 
 const ListItem = ()=>{
+    const data = {
+        discountedPrice:340,
+        price:450,
+        title: "Title of the item",
+        thumbnail:"placeholder.png"
+    }
     return (
         <div className={"item-card"}>
-            <img className={"img-fluid"} src="../../../assets/placeholder.png" alt="some title" />
+            <img className={"img-fluid"} src={`../../../assets/${data.thumbnail}`} alt="some title" />
             <div className={"item-card__information"}>
                 <div className={"pricing"}>
-                    <span>₹340</span>
+                    <span>₹{data.discountedPrice}</span>
                     <small>
-                       <strike>450</strike>
+                       <strike>₹ {data.price}</strike>
                      </small>
                 </div>
                 <div className="title">
-                    <h3>Title of the item </h3>
+                    <h3>{data.title} </h3>
                 </div>
             </div>
             <button className={"cart-add"}>
